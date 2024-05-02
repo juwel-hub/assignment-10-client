a28fb0a2e4033029637761aaf911df9eae6724a9
juwel-hub <jr3764920@gmail.com>
2024-05-02 21:34:23 +0600

    final commit

--- src/Components/OurTeams/OurTeams.jsx
+++ src/Components/OurTeams/OurTeams.jsx
@@ -6,7 +6,7 @@ import { FaSquareWhatsapp } from "react-icons/fa6";
 const OurTeams = () => {
   return (
     <div className="mb-10">
-      <h1 className="text-4xl font-bold text-center my-5">Your Team</h1>
+      <h1 className="text-4xl font-bold text-center my-5">Our Team</h1>
       <div className="md:flex items-center m-3 gap-5">
         <div className=" border p-5 shadow-lg rounded-lg">
           <img src="/10-1.jpg" alt="" />
@@ -46,7 +46,7 @@ const OurTeams = () => {
         </div>
         <div className=" border p-5 shadow-lg rounded-lg">
           <img src="/12-1.jpg" alt="" />
-          <h1 className="text-xl font-bold mt-3">Contact us with</h1>
+          <h1 className="text-xl font-bold mt-3">Contact us with Expert</h1>
           <h1 className="flex items-center gap-5 text-2xl my-3 text-orange-300">
             <a href="">
               <FaInstagramSquare />
